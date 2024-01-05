c84de770baf080da4f39c63b6e44f20b2a5db4ec
RAHUL KUMAR PARIDA <147782697+Rahul-Kumar-Parida@users.noreply.github.com>
2024-01-05 20:09:06 +0530

    add new file

---
+++ JS PROJECT 1/index.js
@@ -0,0 +1,30 @@
+
+let saveEL = document.querySelector(".savepara");
+let counter = document.querySelector(".head2");
+let count = 0;
+function increment() {
+  count = count + 1;
+//   console.log(count);
+document.querySelector(".head2").textContent = count;
+}
+document.querySelector(".increment").addEventListener("click", increment);
+function decrement() {
+  count = count - 1;
+//   console.log(count);
+document.querySelector(".head2").textContent = count;
+}
+document.querySelector(".decrement").addEventListener("click", decrement);
+
+function save(){
+    let countstr = count + " / ";
+    saveEL.textContent +=countstr;
+    counter.textContent = 0;
+    count = 0;
+}
+document.querySelector(".save").addEventListener("click", save);
+
+function reset(){
+  document.querySelector(".savepara").value = "";
+  
+}
+document.querySelector(".reset").addEventListener("click", reset);
